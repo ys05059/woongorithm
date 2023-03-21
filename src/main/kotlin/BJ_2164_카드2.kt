7ed1db043abade46e0cac5f7eb7947d0bb15f13a
ys05059 <ys05059@gmail.com>
2023-03-21 09:29:20 +0900

    03-21-Q1

---
+++ src/main/kotlin/BJ_2164_카드2.kt
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.ArrayDeque
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val deq =ArrayDeque<Int>()
+    repeat(readLine().toInt()){
+        deq.addLast(it+1)
+    }
+    while(deq.size>1){
+        deq.removeFirst()
+        deq.addLast(deq.removeFirst())
+    }
+    println(deq.removeFirst())
+}
+
+// ArrayDeque를 import 하냐에 따라서 메모리랑 속도가 많이 차이남
+// -> 클래스를 찾는 과정에서 불필요한 시간 많이 소모함
