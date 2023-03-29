5206f99bed0dd9e2442bf26c2f97b595de8c3acc
ys05059 <ys05059@gmail.com>
2023-03-29 14:34:44 +0900

    3/22 Q1

---
+++ src/main/kotlin/prefixSum/BJ_11659_구간합구하기4.kt
@@ -0,0 +1,32 @@
+package prefixSum
+
+
+/*
+fold 개념 익히기 및 runningFold 사용해보기
+ */
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    var stk = StringTokenizer(readLine())
+    val n = stk.nextToken().toInt()
+    val m = stk.nextToken().toInt()
+
+    stk = StringTokenizer(readLine())
+    val psum = IntArray(n+1)
+    var temp = 0
+    for (i in 1..n){
+        psum[i] += stk.nextToken().toInt() + temp
+        temp = psum[i]
+    }
+//    println(psum.joinToString(","))
+    val sb = StringBuilder()
+
+    repeat(m){
+        stk = StringTokenizer(readLine())
+        val i = stk.nextToken().toInt()
+        val j = stk.nextToken().toInt()
+        sb.appendLine(psum[j]- psum[i-1])
+    }
+    println(sb.toString())
+}
