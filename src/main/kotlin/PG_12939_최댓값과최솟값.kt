919d92eea207fd5c0337eac71b12821681feab21
ys05059 <ys05059@gmail.com>
2024-02-02 14:42:33 +0900

    PG_12939_최댓값과최솟값

---
+++ src/main/kotlin/PG_12939_최댓값과최솟값.kt
@@ -0,0 +1,7 @@
+// 정렬해서 양 끝값
+class Solution {
+    fun solution(s: String): String {
+        val list = s.split(" ").map{it.toInt()}.sorted()
+        return "${list.first()} ${list.last()}"
+    }
+}
