886be6a15aeecea53987af0fd7f256031ec759c9
ys05059 <ys05059@gmail.com>
2024-02-08 15:31:44 +0900

    [Greedy] BJ_1931_회의실배정

---
+++ src/main/kotlin/BJ_1931_회의실배정.kt
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/source/66708580 참고
+import java.util.PriorityQueue
+import java.util.StringTokenizer
+
+val br = System.`in`.bufferedReader()
+fun main() = with(br){
+
+    val n = readLine().toInt()
+    val pq = PriorityQueue<Pair<Int,Int>>(compareBy({it.second},{it.first}))
+    repeat(n){
+        val st = StringTokenizer(readLine())
+        pq.add(st.nextToken().toInt() to st.nextToken().toInt())
+    }
+    var result = 1
+    var end = pq.poll().second
+    while(pq.isNotEmpty()){
+        val temp = pq.poll()
+        if(temp.first >=end){
+            end = temp.second
+            result++
+        }
+    }
+    println(result)
+}
