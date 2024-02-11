77e0513691eb760dd4eb3e65c73c5527bfb10ca1
ys05059 <ys05059@gmail.com>
2024-02-11 17:31:53 +0900

    [Math] BJ_2292_벌집

---
+++ src/main/kotlin/BJ_2292_벌집.kt
@@ -0,0 +1,15 @@
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+    if(n==1) {
+        println(1)
+        return
+    }
+    var result = 1
+    var min = 2
+    while(min <= n){
+        min += (6*result)
+        result++
+    }
+    println(result)
+}
