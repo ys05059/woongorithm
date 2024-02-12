6c2929e0bad3469d94234bd1d3fe56f4918d76c6
ys05059 <ys05059@gmail.com>
2024-02-12 23:26:29 +0900

    [DP] BJ_17626_FourSquares

---
+++ src/main/kotlin/BJ_17626_FourSquares.kt
@@ -0,0 +1,17 @@
+
+fun main() = with(System.`in`.bufferedReader()){
+    val n = readLine().toInt()
+    val ary = IntArray(n+1)
+    ary[1] = 1
+    var min = 0
+    for (i in 2..n){
+        min = Int.MAX_VALUE
+        var j = 1
+        while (j * j <= i) {
+            min = min.coerceAtMost(ary[i - j * j])
+            j++
+        }
+        ary[i] = min+1
+    }
+    println(ary[n])
+}
