8ddb59c84041bcb79d52dad50f1ecc8c41245695
ys05059 <ys05059@gmail.com>
2024-02-15 16:37:19 +0900

    update BJ_21317_징검다리건너기

--- src/main/kotlin/dp(dynamic programming)/BJ_21317_징검다리건너기.kt
+++ src/main/kotlin/dp(dynamic programming)/BJ_21317_징검다리건너기.kt
@@ -3,16 +3,16 @@ package `dp(dynamic programming)`
 import java.util.StringTokenizer
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    val ary = Array(n + 4) { 0 to 0 }
+    val ary = Array(n + 3) { 0 to 0 }
     for(i in 1 until n){
         val st = StringTokenizer(readLine())
         ary[i] = st.nextToken().toInt() to st.nextToken().toInt()
     }
     val k = readLine().toInt()
-    val noBigJump = IntArray(n + 4){1_000_000}
-    val bigJump = IntArray(n + 4){1_000_000}
+    val noBigJump = IntArray(n + 3){100_000}
+    val bigJump = IntArray(n + 3){100_000}
     noBigJump[1] = 0
-    for (i in 1..n) {
+    for (i in 1 until n) {
         noBigJump[i + 1] = minOf(noBigJump[i + 1], noBigJump[i] + ary[i].first)
         noBigJump[i + 2] = minOf(noBigJump[i + 2], noBigJump[i] + ary[i].second)
         bigJump[i + 3] = minOf(bigJump[i + 2] + ary[i + 2].first, bigJump[i + 1] + ary[i + 1].second, noBigJump[i] + k)
