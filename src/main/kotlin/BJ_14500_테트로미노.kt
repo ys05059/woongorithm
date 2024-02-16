6197cf9aef40fe93b475c50864f32dfae3b86926
ys05059 <ys05059@gmail.com>
2024-02-16 16:51:29 +0900

    update BJ_14500_테트로미노 - 백트랙킹 로직 추가

--- src/main/kotlin/BJ_14500_테트로미노.kt
+++ src/main/kotlin/BJ_14500_테트로미노.kt
@@ -1,5 +1,3 @@
-
-
 /**
  * 음 그냥 4칸 움직였을때 총합이 최대인것 찾으면 되는거 아님? 맞음
  * DFS에 count만 넘겨주면 걍 풀릴듯?
@@ -11,41 +9,40 @@ fun main() = with(System.`in`.bufferedReader()){
     val (n,m) = readLine().split(" ").map { it.toInt() }
     val graph = Array(n){ IntArray(m) }
     val visited = Array(n){BooleanArray(m){false}}
+    val dx = intArrayOf(1,0,-1,0)
+    val dy = intArrayOf(0,-1,0,1)
+    var answer = 0
+    var max = 0
+
     repeat(n){i ->
         val st = StringTokenizer(readLine())
         repeat(m){ j ->
             graph[i][j] = st.nextToken().toInt()
+            max = max.coerceAtLeast(graph[i][j])
         }
     }
-    val dx = intArrayOf(1,0,-1,0)
-    val dy = intArrayOf(0,-1,0,1)
 
-    var max = 0
 
     fun dfs(x : Int, y : Int, cnt : Int, sum : Int){
         if(cnt == 4){
-            max = max.coerceAtLeast(sum)
+            answer = answer.coerceAtLeast(sum)
             return
         }
+        if(answer >= sum + max * (4-cnt)) return
+
         visited[y][x] = true
-        val dir = mutableListOf<Pair<Int,Int>>()
         for(i in  0 until 4){
             val ny = y + dy[i]
             val nx = x + dx[i]
             if(ny in 0 until n && nx in 0 until m && !visited[ny][nx]){
+                // ㅗ 모양 처리
+                if(cnt == 2){
+                    visited[ny][nx] = true
+                    dfs(x,y, cnt +1, sum + graph[ny][nx])
+                    visited[ny][nx] = false
+                    visited[y][x] = true
+                }
                 dfs(nx, ny, cnt+1, sum + graph[ny][nx])
-                dir.add(nx to ny)
-            }
-        }
-        // ㅗ 모양 처리
-        if(cnt == 2){
-            if(dir.size == 2){
-                max = max.coerceAtLeast(sum + graph[dir[0].second][dir[0].first] + graph[dir[1].second][dir[1].first] )
-            }
-            if(dir.size == 3){
-                max = max.coerceAtLeast(sum + graph[dir[0].second][dir[0].first] + graph[dir[1].second][dir[1].first] )
-                max = max.coerceAtLeast(sum + graph[dir[0].second][dir[0].first] + graph[dir[2].second][dir[2].first] )
-                max = max.coerceAtLeast(sum + graph[dir[1].second][dir[1].first] + graph[dir[2].second][dir[2].first] )
             }
         }
         visited[y][x] = false
@@ -56,5 +53,5 @@ fun main() = with(System.`in`.bufferedReader()){
             dfs(x = j,y = i,1, graph[i][j])
         }
     }
-    println(max)
+    println(answer)
 }
