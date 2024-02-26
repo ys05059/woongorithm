e728213ea7fc1ff736f2e4d2bc167622c139a857
ys05059 <ys05059@gmail.com>
2024-02-26 17:49:50 +0900

    [Math] BJ_4153_직각삼각형

---
+++ src/main/kotlin/BJ_4153_직각삼각형.kt
@@ -0,0 +1,9 @@
+import kotlin.math.pow
+fun main() = with(System.`in`.bufferedReader()){
+    while (true){
+        val input = readLine().split(" ").map { it.toDouble() }.sorted()
+        if (input[0].toInt() == 0) break
+        if (input[0].pow(2) + input[1].pow(2) == input[2].pow(2)) println("right") else println("wrong")
+    }
+
+}
