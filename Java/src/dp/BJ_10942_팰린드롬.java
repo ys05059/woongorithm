ff67b482e6c7a41f6ec0f59e4f982bf054016d99
woongyee <ys05059@gmail.com>
2024-09-06 19:29:00 +0900

    [DP]BJ_10942_팰린드롬

--- Java/src/dp/BJ_10942_팰린드롬.java
+++ Java/src/dp/BJ_10942_팰린드롬.java
@@ -1,3 +1,5 @@
+package dp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,38 +7,33 @@ import java.io.StreamTokenizer;
 
 public class BJ_10942_팰린드롬 {
     private static StreamTokenizer st;
-    private static int[] ary;
     public static void main(String[] args) throws IOException {
         st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
         StringBuilder sb = new StringBuilder();
         int n = input();
-        ary = new int[n+1];
+        int[] ary = new int[n+1];
+        boolean[][]dp = new boolean[n+1][n+1];
         for(int i = 1; i <= n; i++){
             ary[i] = input();
+            dp[i][i] = true;
         }
+        for(int i = 1; i < n; i++){                     // i는 검사하는 수의 개수
+            for(int l = 1; l <= n-i; l++){
+                int r = l + i;
+                if(ary[l] == ary[r] && (i == 1 || dp[l+1][r-1])){
+                    dp[l][r] = true;
+                }
+            }
+        }
+
         int m = input();
         while(m > 0){
-            if(check(input(),input())) sb.append("1");
+            if(dp[input()][input()]) sb.append("1");
             else sb.append("0");
             sb.append("\n");
             m--;
         }
         System.out.println(sb);
-    }
-
-    private static boolean check(int a, int b){
-        boolean flag = true;
-        int l = a;
-        int r = b;
-        while(l<r){
-            if(ary[l] != ary[r]){
-                flag = false;
-                break;
-            }
-            l++;
-            r--;
-        }
-        return flag;
     }
 
     private static int input() throws IOException {
